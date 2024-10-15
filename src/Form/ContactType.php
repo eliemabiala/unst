c72e7f39773d42575ee47baab042093d1d043957
mabialaelie4@gmail.com <mabialaelie4@gmail.com>
2024-10-15 21:52:03 +0200

    Added terms and conditions template and modified ContactController and ContactType

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,8 +5,12 @@ namespace App\Form;
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class ContactType extends AbstractType
 {
@@ -15,15 +19,28 @@ class ContactType extends AbstractType
         $builder
             ->add('name', TextType::class, [
                 'attr' => ['class' => 'form-control'],
+                'label' => 'Nom',
             ])
-            ->add('email', TextType::class, [
+            ->add('email', EmailType::class, [
                 'attr' => ['class' => 'form-control'],
+                'label' => 'Email',
             ])
             ->add('subject', TextType::class, [
                 'attr' => ['class' => 'form-control'],
+                'label' => 'Sujet',
             ])
-            ->add('message', TextType::class, [
+            ->add('message', TextareaType::class, [
                 'attr' => ['class' => 'form-control'],
+                'label' => 'Message',
+            ])
+            ->add('termsAccepted', CheckboxType::class, [
+                'mapped' => false, // Ce champ ne sera pas enregistré dans la base de données
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter nos termes et conditions pour continuer.',
+                    ]),
+                ],
+                'label' => 'J\'accepte les termes et conditions',
             ]);
     }
 
