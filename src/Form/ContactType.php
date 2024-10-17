8473b11fce7dfd1ba15769d69c365a21f5352680
mabialaelie4@gmail.com <mabialaelie4@gmail.com>
2024-10-17 09:37:24 +0200

    Added Terms and Conditions page with responsive layout and back button

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,9 +5,10 @@ namespace App\Form;
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 
 class ContactType extends AbstractType
 {
@@ -15,16 +16,26 @@ class ContactType extends AbstractType
     {
         $builder
             ->add('name', TextType::class, [
-                'attr' => ['class' => 'form-control'],
+                'label' => 'Nom',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Votre nom'],
             ])
             ->add('email', TextType::class, [
-                'attr' => ['class' => 'form-control'],
+                'label' => 'Email',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Votre adresse email'],
             ])
             ->add('subject', TextType::class, [
-                'attr' => ['class' => 'form-control'],
+                'label' => 'Sujet',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Sujet de votre message'],
             ])
-            ->add('message', TextType::class, [
-                'attr' => ['class' => 'form-control'],
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Votre message'],
+            ])
+            ->add('termsAccepted', CheckboxType::class, [
+                'label' => 'J\'accepte les <a href="' . $options['terms_route'] . '" class="text-bg-blue-900 hover:underline">termes et conditions</a>.',
+                'required' => true,
+                'mapped' => true,
+                'label_html' => true,
             ]);
     }
 
