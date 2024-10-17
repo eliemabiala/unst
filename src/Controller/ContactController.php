2188e121d72921957891119a7f831a17e96aa61f
mabialaelie4@gmail.com <mabialaelie4@gmail.com>
2024-10-17 08:40:21 +0200

    Up{% extends 'base.html.twig' %}updated contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,20 +8,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
-use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/terms-conditions', name: 'terms_conditions')]
-    public function show(): Response
-    {
-        return $this->render('terms/conditions.html.twig', [
-            'controller_name' => 'TermsController',
-        ]);
-    }
-    
     #[Route('/contact', name: 'app_contact')]
     public function index(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
@@ -31,44 +23,43 @@ class ContactController extends AbstractController
         // Création du formulaire de contact
         $form = $this->createForm(ContactType::class, $contact);
 
-        // Traitement de la requête
+        // Traitement de la requête HTTP pour récupérer les données soumises
         $form->handleRequest($request);
 
         // Vérification si le formulaire est soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
-            // Sauvegarde du contact en base de données
+            // Sauvegarde du contact dans la base de données
             $entityManager->persist($contact);
             $entityManager->flush();
 
-            // Création de l'email
+            // Création de l'email de notification
             $email = (new Email())
-                ->from($this->getParameter('mailer_from')) // Utilise l'adresse email définie dans .env
-                ->to($this->getParameter('mailer_to')) // Utilise l'adresse email définie dans .env
-                ->subject('Nouveau message reçu')
-                ->text('Vous avez un nouveau message.')
-                ->html($this->renderView('emails/notificationcontact.html.twig',
-                    [
-                        'contact' => $contact,
-                    ]
-                ));
+                ->from('mabialaelie4@gmail.com') // L'adresse email de l'expéditeur
+                ->to('endiepro4@gmail.com') // L'adresse email du destinataire
+                ->subject('Nouveau utilisateur ajouté')
+                ->html($this->renderView('emails/notification_contact.html.twig', [
+                    'contact' => $contact
+                ])); // Envoi des données du contact au template
 
-            // Envoi de l'email
+            // Envoi de l'email via le service de mailer
             $mailer->send($email);
 
-
-            // Envoi de l'email
-            $mailer->send($email);
-
-            // Ajout d'un message flash de succès
+            // Ajout d'un message flash de succès après soumission
             $this->addFlash('success', 'Votre message a bien été envoyé.');
 
-            // Redirection vers la même page (ou une autre page si nécessaire)
+            // Redirection vers la même page pour éviter la soumission multiple du formulaire
             return $this->redirectToRoute('app_contact');
         }
 
-        // Rendu du formulaire dans le template
+        // Rendu du formulaire de contact dans la vue
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/conditions', name: 'terms_conditions')]
+    public function termsConditions(): Response
+    {
+        return $this->render('contact/terms_conditions.html.twig');
     }
 }
