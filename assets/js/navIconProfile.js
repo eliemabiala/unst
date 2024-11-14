ec0fb524becc605890f726f3da2a219e042c3f70
mabialaelie4@gmail.com <mabialaelie4@gmail.com>
2024-11-14 21:08:26 +0100

    add modif appoitment

--- assets/js/navIconProfile.js
+++ assets/js/navIconProfile.js
@@ -9,3 +9,22 @@ const profileMenu = document.querySelector('.nav_liste_content_iconprofile');
         });
     }
 });
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const profileIcon = document.querySelector('.nav_icon_profile'); 
+//     const profileMenu = document.querySelector('.nav_liste_content_iconprofile');
+
+//     if (profileIcon && profileMenu) {
+//         profileIcon.addEventListener('click', function(event) {
+//             event.stopPropagation();
+//             profileMenu.classList.toggle('active'); 
+//         });
+
+//         document.addEventListener('click', function(event) {
+//             if (!profileMenu.contains(event.target) && !profileIcon.contains(event.target)) {
+//                 profileMenu.classList.remove('active');
+//             }
+//         });
+//     }
+// });
